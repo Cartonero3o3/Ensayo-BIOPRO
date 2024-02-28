453491ef3bcb8c7585bdad341bd66693e34d296b
Tmucho0 <williamstomas2610@gmail.com>
2024-02-27 23:24:08 -0300

    Hacemos el If de comparacion de id's

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const imgElements = document.getElementsByClassName("img");
+const imgIguales = document.getElementsByClassName("imgD");
+let ultimoClickeado = null
 
 for (let i = 0; i < imgElements.length; i++) {
+    // El addEventListener escucha constantemente si uno de los elementos de la izquierda fue clickeado
     imgElements[i].addEventListener('click', () => {
-        imgElements[i].style.display = "none";
+        // Se asigna el valor del elemento clickeado a ultimoClickeado
+        ultimoClickeado = imgElements[i]
+    });
+}
+
+//Este for lo que hace es recorrer todas las img que estan a la derecha que fueron nombradas como "imgIguales"
+for (let j = 0; j < imgIguales.length; j++) {
+    //Esta linea de abajo escucha cuando se clikee los cuadraditos derechos 
+    imgIguales[j].addEventListener('click', () => {
+        //En eseste if se pregunta iultimo cliceado tiene algo guardado, y despues se pregunta si el nuevo clikeado es igual al ultimo clikeado
+        if (ultimoClickeado !== null && imgIguales[j].id === ultimoClickeado.id) {
+            imgIguales[j].style.display = "none";
+            imgElements[j].style.display = "none";
+        }
     });
 }
