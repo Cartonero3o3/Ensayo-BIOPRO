51fd9ebeb4973436a1c61e8d73ec8b266719d47c
Caba <kirito98.1234@gmail.com>
2024-03-10 19:07:28 -0300

    agregamos las imagenes en orden aleatorio con tomi y juli

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const imgElements = document.getElementsByClassName("img");
-const imgIguales = document.getElementsByClassName("imgD");
+const imgElements = document.getElementsByClassName("Cuadrado");
+const imgIguales = document.getElementsByClassName("CuadradoD");
+const divsIzquierda = document.querySelectorAll('.Cuadrado');
+const divsDerecha = document.querySelectorAll('.CuadradoD');
 let clickCorrecto = document.getElementsByClassName("contadorIgual")[0];
 let clickIncorrecto = document.getElementsByClassName("contadorIncorrecto")[0];
+let mesclador = document.getElementById("mesclador"); 
 let ultimoClickeado= null;
 let contadorCorrecto = 0;
 let contadorIncorrecto = 0;
@@ -9,6 +12,91 @@ let contadorIncorrecto = 0;
 /*
 http://127.0.0.1:5500/index.html
 */
+
+const listaIzquierda = {
+    lista1: [
+        {
+            url: "public/img/celular.jpeg",
+            value: '4'
+        },
+        {
+            url:'public/img/compu.jpeg',
+            value: '1'
+        },
+        {
+            url:'public/img/pierna.jpeg',
+            value: '2'
+        },
+        {
+            url:'public/img/vaso.jpeg',
+            value: '3'
+        }
+    ],
+    lista2: [
+        {
+            url: "public/img/celularGod.jpeg",
+            value: '4'
+        },
+        {
+            url:'public/img/compuGod.jpeg',
+            value: '1'
+        },
+        {
+            url:'public/img/ModeloExactoPierna.jpeg',
+            value: '2'
+        },
+        {
+            url:'public/img/vasoFalso.jpeg',
+            value: '3'
+        }
+    ]
+};
+
+const mesclacion = (arr) => {
+    // Utiliza una función de comparación para ordenar aleatoriamente el array
+    arr.sort(() => Math.random() - 0.5);
+}
+function mesclar() {
+    mesclacion(listaIzquierda.lista1);
+    mesclacion(listaIzquierda.lista2);
+
+    // Asignar imágenes a los elementos
+    divsIzquierda.forEach(function(botonI, i) {
+        let materialI = listaIzquierda.lista1[i];
+        botonI.style.backgroundImage = `url(${materialI.url})`;
+        botonI.setAttribute('value', materialI.value);
+    });
+
+    divsDerecha.forEach(function(botonI, i) {
+        let materialI = listaIzquierda.lista2[i];
+        botonI.style.backgroundImage = `url(${materialI.url})`;
+        botonI.setAttribute('value', materialI.value);
+    });
+}
+
+// Función para mostrar las imágenes al cargar la página
+function mostrarImagenes() {
+    mesclacion(listaIzquierda.lista1);
+    mesclacion(listaIzquierda.lista1);
+    mesclacion(listaIzquierda.lista1);
+    divsIzquierda.forEach(function(botonI, i) {
+        let materialI = listaIzquierda.lista1[i];
+        botonI.style.backgroundImage = `url(${materialI.url})`;
+        botonI.setAttribute('value', materialI.value);
+    });
+    mesclacion(listaIzquierda.lista2);
+    mesclacion(listaIzquierda.lista2);
+    mesclacion(listaIzquierda.lista2);
+    divsDerecha.forEach(function(botonI, i) {
+        let materialI = listaIzquierda.lista2[i];
+        botonI.style.backgroundImage = `url(${materialI.url})`;
+        botonI.setAttribute('value', materialI.value);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", mostrarImagenes);
+mesclador.addEventListener("click", mesclar);
+
 for (let i = 0; i < imgElements.length; i++) {
     // El addEventListener escucha constantemente si uno de los elementos de la izquierda fue clickeado
     imgElements[i].addEventListener('click', () => {
@@ -35,4 +123,6 @@ for (let j = 0; j < imgIguales.length; j++) {
             } else {
                 contadorIncorrecto++;
                 clickIncorrecto.innerText = `Clics Incorrectos: ${contadorIncorrecto}`;
-            }})}
+            }
+    })
+}
