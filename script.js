ef09fe06049050d982f32d9ed8ab2c9c7bc4e6ed
Tmucho0 <williamstomas2610@gmail.com>
2024-03-10 23:48:18 -0300

    Animacion "Shake" para imagenes incorrectas

--- script.js
+++ script.js
@@ -9,10 +9,6 @@ let contadorCorrecto = 0;
 let contadorIncorrecto = 0;
 let CheckList = [0, 0]
 
-/*
-http://127.0.0.1:5500/index.html
-*/
-
 const listaIzquierda = {
     lista1: [
         {
@@ -53,14 +49,13 @@ const listaIzquierda = {
 };
 
 const mesclacion = (arr) => {
-    // Utiliza una función de comparación para ordenar aleatoriamente el array
     arr.sort(() => Math.random() - 0.5);
 }
+
 function mesclar() {
     mesclacion(listaIzquierda.lista1);
     mesclacion(listaIzquierda.lista2);
 
-    // Asignar imágenes a los elementos
     divsIzquierda.forEach(function(botonI, i) {
         let materialI = listaIzquierda.lista1[i];
         botonI.style.backgroundImage = `url(${materialI.url})`;
@@ -74,7 +69,6 @@ function mesclar() {
     });
 }
 
-// Función para mostrar las imágenes al cargar la página
 function mostrarImagenes() {
     mesclacion(listaIzquierda.lista1);
     mesclacion(listaIzquierda.lista1);
@@ -97,31 +91,33 @@ function mostrarImagenes() {
 document.addEventListener("DOMContentLoaded", mostrarImagenes);
 mesclador.addEventListener("click", mesclar);
 
-//Este for lo que hace es recorrer todas las img que estan a la derecha que fueron nombradas como "imgIguales"
-// Escucha los clics en los cuadrados izquierdos
 for (let j = 0; j < CuadradosIzquierdos.length; j++) {
     CuadradosIzquierdos[j].addEventListener('click', () => {
         CheckList[0] = divsIzquierda[j].getAttribute('value');
-        // Verifica si ambos lados tienen valores asignados y si son iguales
         if (CheckList[0] != 0 && CheckList[1] != 0 ) {
             if (CheckList[1] === CheckList[0]) {
                 contadorCorrecto++;
                 clickCorrecto.innerText = `Clics Iguales: ${contadorCorrecto}`;
                 CheckList = [0, 0]
+                
             } else {
                 contadorIncorrecto++;
                 clickIncorrecto.innerText = `Clics Incorrectos: ${contadorIncorrecto}`;
+                // Agregamos la clase animate__shakeX para la animación de sacudida
+                document.body.classList.add('animate__animated', 'animate__shakeX');
+                // Quitamos la clase después de un tiempo para que la animación no se repita
+                setTimeout(() => {
+                    document.body.classList.remove('animate__animated', 'animate__shakeX');
+                }, 2000);
                 CheckList = [0, 0]
             }
         }
     });
 }
 
-// Escucha los clics en los cuadrados derechos
 for (let i = 0; i < CuadradosDerechos.length; i++) {
     CuadradosDerechos[i].addEventListener('click', () => {
         CheckList[1] = divsDerecha[i].getAttribute('value');
-        // Verifica si ambos lados tienen valores asignados y si son iguales
         if (CheckList[0] != 0 && CheckList[1] != 0 ) {
             if (CheckList[1] === CheckList[0]) {
                 contadorCorrecto++;
@@ -130,32 +126,12 @@ for (let i = 0; i < CuadradosDerechos.length; i++) {
             } else {
                 contadorIncorrecto++;
                 clickIncorrecto.innerText = `Clics Incorrectos: ${contadorIncorrecto}`;
+                document.body.classList.add('animate__animated', 'animate__shakeX');
+                setTimeout(() => {
+                    document.body.classList.remove('animate__animated', 'animate__shakeX');
+                }, 1000);
                 CheckList = [0, 0]
             }
         }
     });
 }
-
-
-/*
-for (let j = 0; j < imgIguales.length; j++) {
-    //Esta linea de abajo escucha cuando se clikee los cuadraditos derechos 
-    imgIguales[j].addEventListener('click', () => {
-            //En eseste if se pregunta iultimo cliceado tiene algo guardado, y despues se pregunta si el nuevo clikeado es igual al ultimo clikeado
-            if (ultimoClickeado !== null && imgIguales[j].id === ultimoClickeado.id) {
-                imgIguales[j].style.opacity = 0;
-                imgElements[j].style.opacity = 0;
-                contadorCorrecto++;
-                clickCorrecto.innerText = `Clics Iguales: ${contadorCorrecto}`;
-                setTimeout(() => {
-                  // Oculta los elementos después de que la animación haya terminado
-                imgIguales[j].style.display = "none";
-                imgElements[j].style.display = "none";
-                }, 300);
-            } else {
-                contadorIncorrecto++;
-                clickIncorrecto.innerText = `Clics Incorrectos: ${contadorIncorrecto}`;
-            }
-    })
-}
-*/
